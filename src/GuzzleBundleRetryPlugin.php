1eac2fa561b3012097ba103b1791d256159ab422
Eugen Ganshorn <eugen@ganshorn.eu>
2019-06-05 21:48:14 +0200

    initial commit

---
+++ src/GuzzleBundleRetryPlugin.php
@@ -0,0 +1,118 @@
+<?php
+
+namespace EugenGanshorn\Bundle\GuzzleBundleRetryPlugin;
+
+use EightPoints\Bundle\GuzzleBundle\EightPointsGuzzleBundlePlugin;
+use GuzzleRetry\GuzzleRetryMiddleware;
+use Symfony\Component\Config\Definition\Builder\ArrayNodeDefinition;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Definition;
+use Symfony\Component\DependencyInjection\Reference;
+use Symfony\Component\ExpressionLanguage\Expression;
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class GuzzleBundleRetryPlugin extends Bundle implements EightPointsGuzzleBundlePlugin
+{
+    /**
+     * The name of this plugin. It will be used as the configuration key.
+     *
+     * @return string
+     */
+    public function getPluginName(): string
+    {
+        return 'retry';
+    }
+
+    /**
+     * @param \Symfony\Component\Config\Definition\Builder\ArrayNodeDefinition $pluginNode
+     *
+     * @return void
+     */
+    public function addConfiguration(ArrayNodeDefinition $pluginNode)
+    {
+        $pluginNode
+            ->canBeEnabled()
+            ->children()
+                ->scalarNode('retry_enabled')
+                ->defaultTrue()
+            ->end()
+                ->scalarNode('max_retry_attempts')
+                ->defaultValue(10)
+            ->end()
+                ->scalarNode('retry_only_if_retry_after_header')
+                ->defaultFalse()
+            ->end()
+                ->arrayNode('retry_on_status')
+                ->defaultValue([503, 429])
+                    ->scalarPrototype()
+                ->end()
+            ->end()
+                ->scalarNode('default_retry_multiplier')
+                ->defaultValue(1.5)
+            ->end()
+                ->scalarNode('retry_on_timeout')
+                ->defaultFalse()
+            ->end()
+                ->scalarNode('expose_retry_header')
+                ->defaultFalse()
+            ->end()
+                ->scalarNode('retry_header')
+                ->defaultValue('X-Retry-Counter')
+            ->end()
+        ->end();
+    }
+
+    /**
+     * Load this plugin: define services, load service definition files, etc.
+     *
+     * @param array            $configs
+     * @param ContainerBuilder $container
+     *
+     * @return void
+     */
+    public function load(array $configs, ContainerBuilder $container)
+    {
+    }
+
+    /**
+     * Add configuration nodes for this plugin to the provided node.
+     *
+     * @param array            $config
+     * @param ContainerBuilder $container
+     * @param string           $clientName
+     * @param Definition       $handler
+     *
+     * @return void
+     */
+    public function loadForClient(array $config, ContainerBuilder $container, string $clientName, Definition $handler)
+    {
+        if ($config['retry_enabled']) {
+            $logger = new Definition(Logger::class);
+            $logger->addMethodCall('setLogger', [new Reference('monolog.logger.eight_points_guzzle')]);
+            $logger->addMethodCall('setFormatter', [new Reference('eight_points_guzzle.symfony_log_formatter')]);
+
+            $middleware = new Definition(GuzzleRetryMiddleware::class);
+            $middleware->setFactory([GuzzleRetryMiddleware::class, 'factory']);
+            $middleware->setArguments([
+                [
+                    'max_retry_attempts'               => $config['max_retry_attempts'],
+                    'retry_only_if_retry_after_header' => $config['retry_only_if_retry_after_header'],
+                    'retry_on_status'                  => $config['retry_on_status'],
+                    'default_retry_multiplier'         => $config['default_retry_multiplier'],
+                    'retry_on_timeout'                 => $config['retry_on_timeout'],
+                    'expose_retry_header'              => $config['expose_retry_header'],
+                    'retry_header'                     => $config['retry_header'],
+                    'on_retry_callback'                => [$logger, 'callback'],
+                ],
+            ]);
+
+            $middleware->setPublic(true);
+
+            $middlewareServiceName = sprintf('guzzle_bundle_retry_plugin.middleware.retry.%s', $clientName);
+            $container->setDefinition($middlewareServiceName, $middleware);
+
+            $middlewareExpression = new Expression(sprintf('service("%s")', $middlewareServiceName));
+            $handler->addMethodCall('push', [$middlewareExpression]);
+        }
+    }
+}
---
+++ src/Logger.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace EugenGanshorn\Bundle\GuzzleBundleRetryPlugin;
+
+use GuzzleHttp\MessageFormatter;
+use GuzzleHttp\Psr7\Request;
+use GuzzleHttp\Psr7\Response;
+use Psr\Log\LoggerAwareInterface;
+use Psr\Log\LoggerAwareTrait;
+
+class Logger implements LoggerAwareInterface
+{
+    use LoggerAwareTrait;
+
+    /**
+     * @var MessageFormatter
+     */
+    protected $formatter;
+
+    public function setFormatter(MessageFormatter $formatter)
+    {
+        $this->formatter = $formatter;
+    }
+
+    public function callback(
+        $attemptNumber,
+        $delay,
+        Request $request,
+        $options,
+        Response $response
+    ) {
+        $this->logger->info(
+            sprintf('%s - will wait %s seconds and try it again, this is attempt #%d',
+                $this->formatter->format($request, $response),
+                number_format($delay, 2),
+                $attemptNumber
+            )
+        );
+    }
+}
